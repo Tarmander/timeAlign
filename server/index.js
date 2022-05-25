6904ff32df793654682b9e6e33d43497987de1f1
Kevin Tarhan <kevtarha@cox.net>
2022-05-25 15:33:57 -0700

    Added MongoDB and mongoose calls, POST now sends data from frontend to the DB

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const db = require('./db.js')
 const app = express();
 const port = 3000;
 
-app.use(express.static('./client'));
+app.use(express.static('./client/html', {extensions:['html']}));
 app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Zonelink listening on port ${port}`)
+  db.connectToDB();
+});
 
 app.get('/api', async (req, res) => {
   res.send('Get World!')
 });
 
 app.post('/api', async (req, res) => {
-    console.log(req.body);
+  db.save(req.body);
 });
 
-app.listen(port, () => {
-  console.log(`Zonelink listening on port ${port}`)
-});
+
 
